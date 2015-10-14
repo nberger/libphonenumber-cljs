d8ac960dc81c80f87f29988e47fd98878501031e
Nicolas Berger <nicoberger@gmail.com>
2015-10-14 15:51:00 -0300

    Add deploy repository: clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject nberger/libphonenumber-cljs "7.1.0-SNAPSHOT"
   :description "libphonenumber packaged for cljs"
   :url "http://github.com/nberger/libphonenumber-cljs"
+  :deploy-repositories [["releases" :clojars]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
